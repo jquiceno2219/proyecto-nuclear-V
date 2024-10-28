bfae8acb9ff6f30fcdb951dbd5c1798f70d335aa
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-10-28 14:42:10 -0500

    Librerias

--- front-end/app-vue/src/main.ts
+++ front-end/app-vue/src/main.ts
@@ -1,6 +1,15 @@
 import './assets/main.css'
 
+import 'bootstrap'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 createApp(App).mount('#app')
+
+
